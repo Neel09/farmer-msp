99c11b21ee534bd4b12c9128c42557c95afe4db4
Neel09 <neelansh.gupta080@gmail.com>
2021-01-28 14:48:53 +0530

    Setting up farmer-msp project : Initial Commit

---
+++ src/test/java/org/jai/kisan/jaikisanparent/JaiKisanParentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jai.kisan.jaikisanparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaiKisanParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
